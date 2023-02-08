87f652516fdf020ba35821213b59475b26ba8490
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-02-07 21:46:14 -0300

    variables

---
+++ primerosPasos/src/tipos_datos/TiposDatosNumericos.java
@@ -0,0 +1,17 @@
+package tipos_datos;
+
+import java.awt.KeyEventDispatcher;
+
+public class TiposDatosNumericos {
+	public static void main(String[] args) {
+		int edad = 28;
+		System.out.println(edad);
+		
+		edad = 47;
+		System.out.println(edad);
+		
+		edad = 46 + 88;
+		System.out.println("Mi edad es :"+edad);
+	}
+
+}
